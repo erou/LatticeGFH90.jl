07ce7f6b3f9cd4bb45bd7886f20a66d2b21926e9
Édouard Rousseau <rousseau.edouard@gmail.com>
2019-01-27 16:11:14 +0100

    a lot of benchmarks

--- benchmarks.jl
+++ benchmarks.jl
@@ -72,3 +72,51 @@ function bench_embed(p, N)
     end
     close(io)
 end
+
+function bench_all(p, N)
+
+    path = string("../benchmarks/solve_h90-", p, ".txt")
+    io = open(path, "w+")
+
+    path2 = string("../benchmarks/embed-", p, ".txt")
+    io2 = open(path2, "w+")
+
+    # Compute the H90 sol in A_l with l <= N
+    for j in 1:N
+        if j%p == 0
+            continue
+        end
+        a = level(j, p)
+        if haskey(ZETAS, (p, a))
+
+            println(j)
+            k, x = FiniteField(p, j, "x")
+            A, t = (@timed tensor_algebra(k))[1:2]
+            h, u = (@timed solve_h90(A))[1:2]
+            write(io, string(level(A), ",", degree(A), ",", t+u, "\n"))
+            H90_ELEMENTS[(p, j)] = h
+        end
+    end
+
+    # Sort the A_l
+    degrees = sort!(Int[y for (x,y) in keys(H90_ELEMENTS) if x == p])
+    len = length(degrees)
+
+    # Make all the possible embeddings
+    for j in 1:len
+        for i in (j+1):len
+            l, m = degrees[j], degrees[i]
+            if m % l == 0
+                println(string(l, " ... ", m))
+                k, x = FiniteField(p, l, "x")
+                K, y = FiniteField(p, m, "y")
+                a, b = level(l, p), level(m, p)
+
+                v = @elapsed embed(k, K)
+                write(io2, string(a, ",", l, ",", b, ",", m, ",", v, "\n"))
+            end
+        end
+    end
+    close(io)
+    close(io2)
+end
