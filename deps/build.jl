153fbe091a8559bf3646697a9aeebf21d0bada05
Édouard Rousseau <rousseau.edouard@gmail.com>
2019-02-11 19:57:35 +0100

    is that better NOW?

--- deps/build.jl
+++ deps/build.jl
@@ -3,12 +3,13 @@ using Libdl, Nemo
 old_dir = pwd()
 deps_dir = dirname(@__FILE__)
 nemo_dir = dirname(dirname(pathof(Nemo)))
-flint_dir = joinpath(nemo_dir, "local", "lib")
+flint_lib = joinpath(nemo_dir, "local", "lib")
+flint_headers = joinpath(nemo_dir, "local", "include")
 
 # Make libembed
 
 cd(deps_dir)
-run(`make libembed.so ARGS=$flint_dir`)
+run(`make libembed.so ARG1=$flint_headers ARG2=$flint_dir`)
 
 # Add the path
 
